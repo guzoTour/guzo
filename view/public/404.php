32eb1b285e8d6dcc59885071e1de4b19dd26cb6f
yoni1342 <yonatantesfaye30@gmail.com>
2022-06-07 02:35:11 +0300

    tour

--- view/public/404.php
+++ view/public/404.php
@@ -22,7 +22,7 @@
     <div class="flex flex-col justify-center items-center">
         <h1 class="text-4xl font-bold text-red-500 px-60 mt-20">Page Not Found!</h1>
         <div class="flex items-center w-[1100px]">
-            <img src="../../multimedia/img/travel3.svg" alt="" class="w-96 animate-bounce">
+            <img src="../../multimedia/img/travel3.svg" alt="" class="w-96 ">
             <img src="../../multimedia/img/404.png" alt="" class = 'animate-pulse'>
         </div>
     </div>
